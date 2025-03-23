89b2ae82020e398b1aa1c47053aa384a75e5c732
saburali <saburali.me@gmail.com>
2025-03-24 02:53:08 +0600

    Add Slick Carousel integration and new services section to index.html with styling updates

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
+    });
+});
