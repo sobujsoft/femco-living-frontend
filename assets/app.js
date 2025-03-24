47610ab9b5961f552d7c5623457bdfb742028865
saburali <saburali.me@gmail.com>
2025-03-24 22:16:47 +0600

    Add YouTube video showcase with modal functionality in index.html, app.js, and style.css

--- assets/app.js
+++ assets/app.js
@@ -11,4 +11,23 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
         nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>',
     });
+
+    // Get the modal and iframe
+    var youtubeModal = $('#youtubeModal');
+    var youtubeIframe = $('#youtubeIframe');
+
+    // Add event listener to set the video URL dynamically
+    youtubeModal.on('show.bs.modal', function(event) {
+        // Get the button that triggered the modal
+        var button = $(event.relatedTarget);
+        // Extract the video URL from the data attribute
+        var videoUrl = button.data('video-url');
+        // Set the iframe src to the video URL with autoplay
+        youtubeIframe.attr('src', videoUrl + '?autoplay=1');
+    });
+
+    // Add event listener to clear the video URL when the modal closes
+    youtubeModal.on('hidden.bs.modal', function() {
+        youtubeIframe.attr('src', '');
+    });
 });
